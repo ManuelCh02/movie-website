669d6b332380e24533240012f05a3d3b78c02dd0
manuch <chingayloez4@gmail.com>
2025-05-21 22:26:25 -0500

    Trying dynamic view for loading content on HTML with JS

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,1 +1,27 @@
 // Here's where the logic of singly linked list data structure will work
+import { movieDetails } from "./views.js"
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor(value) {
+        this.top = null
+        this.bottom = value
+        this.length = 0
+    }
+}
+
+const detailBtn = document.querySelector('.container-top__details-btn');
+const body = document.querySelector('body');
+
+detailBtn.addEventListener('click', () => {
+    console.log('button works');
+    const movieDetail = movieDetails();
+    console.log(movieDetail)
+    body.append(movieDetail)
+})
--- src/js/views.js
+++ src/js/views.js
@@ -1,1 +1,41 @@
 // Here's where we'll render all views dynamically
+export function movieDetails() {
+    const body = document.createElement('section');
+    body.innerHTML = 
+    `
+    <header>
+        <div>
+            <button>&lt;</button>
+            <span>Detail</span>
+        </div>
+    </header>
+
+    <main>
+        <section>
+            <img src="https://media.themoviedb.org/t/p/w440_and_h660_face/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg">
+            <div>
+                <span>SERIES</span>
+                <h1>The Last Of Us 2</h1>
+                <ul>
+                    <li>Action</li>
+                    <li>Romance</li>
+                    <li>Adventure</li>
+                    <li>Horror</li>
+                </ul>
+            </div>
+            <p>
+                After a global pandemic destroys civilization, a hardened survivor takes charge of a 14-year-old girl who may be humanity's last hope.
+            </p>
+            <div>
+                <ul>
+                    <li>⭐ 8.8</li>
+                    <li>2025</li>
+                    <li>50min</li>
+                </ul>
+            </div>
+        </section>
+    </main>
+    `;
+
+    return body
+}
