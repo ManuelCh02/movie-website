9c9378cb0eec1cbcbf52a8ea0e02b881bd18e81a
manuch <chingayloez4@gmail.com>
2025-06-10 22:26:57 -0500

    First steps into fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
+// import { API_KEY } from "../sec";
 
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer' + API_KEY,
+    }
+}
+
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day', options);
+    const data = await res.json();
+    
+    // const movies = data.results;
+    console.log({ data })
+}
+
+getTrendingMoviesPreview();
