7d21a07cb2d3e5bc09a064c764430f69daaadbe4
manuch <chingayloez4@gmail.com>
2025-07-02 21:16:24 -0500

    Finished ⭐⭐⭐

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-// Here's where the logic of singly linked list data structure will work
-import { homePage } from "./views.js"
-import { movieDetails } from "./views.js"
-import { seeAllTrending } from "./views.js"
-import { seeAllPlaying } from "./views.js"
-import { seeAllUpcoming } from "./views.js"
-import { searchMovieById } from "./main.js"
-import { getMovieDetailContent } from "./main.js"
-import { seeAllPage } from "./main.js"
-import { seeAllNowPlayingPage } from "./main.js"
-import { seeAllUpcomingPage } from "./main.js"
-=======
 import { getTrendingMoviesPreview } from "./main.js";
 import { getCategoriesPreview } from "./main.js";
 import { getMoviesByCategory } from "./main.js";
@@ -18,7 +5,6 @@ import { getMoviesBySearch } from "./main.js";
 import { getTrendingMovies } from "./main.js";
 import { getMovieById } from "./main.js";
 import { sections } from './views.js';
->>>>>>> platzi-fetch
 
 // Nodes
 const homePageHeader = document.querySelector('#header-home');
@@ -92,23 +78,6 @@ function homePage() {
 function categoriesPage() {
     console.log('CATEGORIES!');
 
-<<<<<<< HEAD
-async function loadHomePage() {
-    const initialLoadingPage = await homePage()
-
-    if(!myStack.length) {
-    myStack.push(initialLoadingPage);
-    appParent.appendChild(myStack.peek().value);
-    }
-}
-
-loadHomePage()
-
-document.body.addEventListener('click', (e) => {
-    if(e.target.matches('.container-top__details-btn')) {
-        updateAppContent(movieDetails());
-    }
-=======
     categoryPageHeader.classList.remove('inactive');
     categoryPageMain.classList.remove('inactive');
 
@@ -131,60 +100,10 @@ document.body.addEventListener('click', (e) => {
 
 function movieDetailsPage() {
     console.log('MOVIE!');
->>>>>>> platzi-fetch
 
     detailPageHeader.classList.remove('inactive');
     detailPageMain.classList.remove('inactive');
 
-<<<<<<< HEAD
-    if(e.target.matches('.trending-now-see-all')) {
-        updateAppContent(seeAllTrending());
-        seeAllPage();
-    }
-
-    if(e.target.matches('.latest-see-all')) {
-        updateAppContent(seeAllPlaying());
-        seeAllNowPlayingPage();
-    }
-
-    if(e.target.matches('.upcoming-see-all')) {
-        updateAppContent(seeAllUpcoming());
-        seeAllUpcomingPage();
-    }
-
-    if(e.target.matches('.movie-img')) {
-        const movieImg = e.target;
-        const getMovieName = { title: movieImg.alt, id: movieImg.id };
-        appendDetailMovieInStack(getMovieName)
-            .then(res => updateAppContent(res))
-            .catch(error => {  
-                console.error('Error -_-: ', error)
-            })
-    }
-})
-
-function updateAppContent(nodeContent) {;
-    myStack.push(nodeContent);
-    appParent.innerHTML = '';
-    appParent.appendChild(myStack.peek().value);
-}
-
-function returnAppContent() {
-    myStack.pop();
-    appParent.innerHTML = '';
-    appParent.appendChild(myStack.peek().value);
-}
-
-async function appendDetailMovieInStack(movieData) {
-    try {
-        const eventDataResponse = await searchMovieById(movieData);
-        const sendInfoToDetails = await getMovieDetailContent(eventDataResponse)
-        const movieDetail = movieDetails(sendInfoToDetails)
-        return movieDetail
-    } catch(error) {
-        console.error('Error yo: ', error)
-    }
-=======
     homePageHeader.classList.add('inactive');
     homePageMain.classList.add('inactive');
     allTrendingPageHeader.classList.add('inactive');
@@ -234,5 +153,4 @@ function trendsPage() {
 
     getTrendingMovies();
     window.scrollTo(0, 0);
->>>>>>> platzi-fetch
 }
